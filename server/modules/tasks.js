12ec98066e9878a0f421e5851a11bd1f14fa24d7
Pete Hall <pete.mack.hall@gmail.com>
2022-05-19 14:05:44 -0500

    Set up POST call to server.

--- server/modules/tasks.js
+++ server/modules/tasks.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+let tasks = [];
 
+router.post('/', (req, res)=>{
+  console.log('in /tasks POST:', req.body);
+  tasks.push(req.body);
+  res.sendStatus(201);
+})
 
 module.exports = router;
