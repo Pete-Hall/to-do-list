1fc4ab0780c031dc08b46eef3cd81e14bea2eb43
Pete Hall <pete.mack.hall@gmail.com>
2022-05-19 13:57:02 -0500

    Set up client.js POST call.

---
+++ server/public/scripts/client.js
@@ -0,0 +1,30 @@
+$(document).ready(onReady);
+
+function onReady() {
+  // event handlers
+  $('#addTaskButton').on('click', addTask);
+}
+
+function addTask() {
+  console.log('in addTask');
+  // store inputted data in an object to send
+  let newTask = {
+    description: $('#tasksIn').val()
+  }
+  console.log('sending:', newTask);
+  // make AJAX POST call to send data to server
+  $.ajax({
+    method: 'POST',
+    url: '/tasks',
+    data: newTask
+  }).then(function(response) {
+    console.log('back from POST:', response);
+    // TODO: display tasks on the DOM
+
+    // clear user inputs
+    $('#tasksIn').val('');
+  }).catch(function(err) {
+    console.log(err);
+    alert('error adding task');
+  })
+}
