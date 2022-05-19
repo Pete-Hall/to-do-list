8cad6f92d4a7fea2667781742d5e1d498209e952
Pete Hall <pete.mack.hall@gmail.com>
2022-05-19 16:33:19 -0500

    Set up READ with GET request.

--- server/modules/tasks.js
+++ server/modules/tasks.js
@@ -1,6 +1,18 @@
+const { query } = require('express');
 const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
+
+router.get('/', (req, res)=>{
+  console.log('in /koalas GET');
+  let queryString = `SELECT * FROM tasks;`;
+  pool.query(queryString).then((results)=>{
+    res.send(results.rows);
+  }).catch((err)=>{
+    console.log(err);
+    res.sendStatus(500);
+  })
+})
 
 router.post('/', (req, res)=>{
   console.log('in /tasks POST:', req.body);
@@ -8,7 +20,7 @@ router.post('/', (req, res)=>{
   let queryString = `INSERT INTO tasks (description) VALUES ($1);`; 
   let values = [req.body.description];
   pool.query(queryString, values).then((results)=>{
-    res.sendStatus(200);
+    res.sendStatus(201);
   }).catch((err)=>{
     console.log(err);
     res.sendStatus(500);
